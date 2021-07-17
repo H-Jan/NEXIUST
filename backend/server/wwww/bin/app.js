ffd003e7eb48f4eb9e219bc5a8e83af51c05f883
OmarJ <omarjandali93@gmail.com>
2021-07-16 20:02:17 -0700

    003 Omar Jandali; Setup the initial server model to test and ensure it runs;

---
+++ backend/server/wwww/bin/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//const database = require('../../../database/index.js');
+// const user = require('../../../database/models/user/User.js');
+// const profile = require('../../../database/models/user/Profile.js');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(morgan('dev'));
+
+module.exports = app;
